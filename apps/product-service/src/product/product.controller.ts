956011b3cd2f0af50e49f8a0016edaeaa0e8c5ab
RatulAlMamun <ratulalmamun23@gmail.com>
2025-05-30 23:05:28 +0600

    chore(product): implement fetch all product with pagination and fetch single product.

--- apps/product-service/src/product/product.controller.ts
+++ apps/product-service/src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
@@ -13,4 +22,27 @@ export class ProductController {
   create(@Body() dto: CreateProductDto, @CurrentUser() user: any) {
     return this.service.create(dto, user.sub);
   }
+
+  @Get()
+  async findAll(
+    @CurrentUser() user: any,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+  ) {
+    return this.service.findAll(user, Number(page), Number(limit));
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() dto: UpdateProductDto,
+  //   @CurrentUser() user: any,
+  // ) {
+  //   return this.service.update(id, dto, user.sub);
+  // }
 }
--- apps/product-service/src/product/product.service.ts
+++ apps/product-service/src/product/product.service.ts
@@ -11,8 +11,30 @@ export class ProductService {
     @InjectModel(Product.name)
     private readonly productModel: ReturnModelType<typeof Product>,
   ) {}
+
   async create(createDto: CreateProductDto, userId: string) {
     const created = new this.productModel({ ...createDto, userId });
-    return created.save();
+    const product = await created.save();
+    return { message: 'Product create successfully!', data: product };
+  }
+
+  async findAll(user: any, page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+
+    const [data, total] = await Promise.all([
+      this.productModel.find().skip(skip).limit(limit),
+      this.productModel.countDocuments(),
+    ]);
+
+    return {
+      message: 'All products',
+      data,
+      total,
+    };
+  }
+
+  async findOne(id: string) {
+    const data = await this.productModel.findById(id);
+    return { message: 'Single product fetch', data };
   }
 }
