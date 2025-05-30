7bbc9b2c0d5dc0702dcbf8341b713a3fbd021fe5
RatulAlMamun <ratulalmamun23@gmail.com>
2025-05-30 21:03:34 +0600

    chore(decorator): implement current user decorator.

--- apps/product-service/src/product/product.controller.ts
+++ apps/product-service/src/product/product.controller.ts
@@ -2,6 +2,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('products')
 @UseGuards(AuthGuard)
@@ -9,7 +10,8 @@ export class ProductController {
   constructor(private readonly service: ProductService) {}
 
   @Get()
-  test() {
+  test(@CurrentUser() user: any) {
+    console.log({ user });
     return this.service.test();
   }
 }
